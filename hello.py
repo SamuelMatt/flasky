38d87c2ab10adb6b2b91fdb13cd7769b1e0312bb
MattMa <ms_achencer@yahoo.com>
2018-02-26 15:02:23 +0800

    Web Form

--- hello.py
+++ hello.py
@@ -1,23 +1,44 @@
-from datetime import datetime
-from flask import Flask, render_template
+# from datetime import datetime
+from flask import Flask, render_template, session, redirect, url_for, flash
 # from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 # manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField("What's your name?", validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        oldName = session.get('name')
+        if oldName is not None and oldName != form.name.data:
+            flash('Look like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template(
+        'index.html',
+        form = form,
+        name = session.get('name'))
 
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template(
+        'user.html',
+        name = name)
 
 
 @app.errorhandler(404)
