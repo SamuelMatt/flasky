33ce3973c633a70767e155eee4e1b3e99f2143ff
MattMa <ms_achencer@yahoo.com>
2018-03-05 17:26:27 +0800

    Email

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
-# from datetime import datetime
-from os import path
+from os import path, environ
+from threading import Thread
 from flask import Flask, render_template, session, redirect, url_for
 from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
@@ -9,28 +9,30 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
-
-
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-app.config['SECRET_KEY'] = 'hard to guess string'
+from flask_mail import Mail, Message
 
 basedir = path.abspath(path.dirname(__file__))
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{path.join(basedir, 'data.sqlite')}"
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-
-def makeShellContex():
-    return dict(app=app, db=db, User=User, Role=Role)
-
+app.config['MAIL_SERVER'] = '127.0.0.1'
+app.config['MAIL_PORT'] = 25
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USERNAME'] = environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = environ.get('MAIL_PASSWORD')
+app.config['FLASK_MAIL_SUBJECT_PREFIX'] = '[Flask]'
+app.config['FLASK_ADMIN'] = environ.get('FLASK_ADMIN')
+app.config['FLASK_MAIL_SENDER'] = f"Flask Admin <{app.config['FLASK_ADMIN']}>"
 
 manager = Manager(app)
-manager.add_command('shell', Shell(make_context=makeShellContex))
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
+mail = Mail(app)
 
 
 class NameForm(FlaskForm):
@@ -58,6 +60,32 @@ class User(db.Model):
         return f'<User {self.username!r}>'
 
 
+def makeShellContex():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+manager.add_command('shell', Shell(make_context=makeShellContex))
+manager.add_command('db', MigrateCommand)
+
+
+def sendAsyncMail(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def sendMail(to, subject, template, **kwargs):
+    msg = Message(
+        subject=app.config['FLASK_MAIL_SUBJECT_PREFIX'] + subject,
+        sender=app.config['FLASK_MAIL_SENDER'],
+        recipients=[to]
+    )
+    msg.body = render_template(f'{template}.txt', **kwargs)
+    msg.html = render_template(f'{template}.html', **kwargs)
+    thr = Thread(target=sendAsyncMail, args=[app, msg])
+    thr.start()
+    return thr
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
@@ -67,11 +95,18 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['know'] = False
+            if app.config['FLASK_ADMIN']:
+                sendMail(
+                    app.config['FLASK_ADMIN'],
+                    'New User',
+                    'mail/new_user',
+                    user=user
+                )
         else:
             session['know'] = True
         session['name'] = form.name.data
         form.name.data = ''
-        return redirect(url_for(index))
+        return redirect(url_for('index'))
     return render_template(
         'index.html',
         form=form,
@@ -100,5 +135,9 @@ def internal_server_error(e):
 
 if __name__ == '__main__':
     manager.run(
-        # debug=True
+        debug=False
     )
+
+    # app.run(
+    #     debug=False
+    # )
